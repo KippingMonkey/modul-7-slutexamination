58de71fcfcdf27004909a0725291eaa89ee93314
Jonas <jonas@kippingmonkey.se>
2022-03-14 17:01:37 +0100

    start work on langing view

--- airbean_react/src/views/Landing.js
+++ airbean_react/src/views/Landing.js
@@ -1,6 +1,15 @@
+import './Landing.css';
+import introGraphicLeft from '../assets/graphics/intro-graphic-left.svg';
+import introGraphicRight from '../assets/graphics/intro-graphic-right.svg';
+import airbeanLogo from '../assets/graphics/airbean-landing.svg';
+
 function Landing () {
   return(
-    <h1>Landing page</h1>
+    <div className='landing-container'>
+      <img src={introGraphicLeft} className="graphic-left"/>
+      <img src={airbeanLogo} className="logo"/>
+      <img src={introGraphicRight} className="graphic-right"/>
+    </div>
   )
 }
 
