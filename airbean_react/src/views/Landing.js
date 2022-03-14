fadd82eb945bb8f3ad59ee25a2253d3e763e7aa3
KippingMonkey <73753894+KippingMonkey@users.noreply.github.com>
2022-03-14 22:42:19 +0100

    finish layout landing. begin layout menu

--- airbean_react/src/views/Landing.js
+++ airbean_react/src/views/Landing.js
@@ -1,15 +1,21 @@
 import './Landing.css';
+
+import { Link } from 'react-router-dom';
 import introGraphicLeft from '../assets/graphics/intro-graphic-left.svg';
 import introGraphicRight from '../assets/graphics/intro-graphic-right.svg';
 import airbeanLogo from '../assets/graphics/airbean-landing.svg';
 
 function Landing () {
   return(
-    <div className='landing-container'>
-      <img src={introGraphicLeft} className="graphic-left"/>
-      <img src={airbeanLogo} className="logo"/>
-      <img src={introGraphicRight} className="graphic-right"/>
-    </div>
+    <Link to="/menu">
+      <div className='landing-container'>
+        <img src={ introGraphicLeft } className="graphic-left"/>
+        <div className='logo-container'>
+          <img src={ airbeanLogo } className="logo" />
+        </div>
+        <img src={ introGraphicRight } className="graphic-right"/>
+      </div>
+    </Link>
   )
 }
 
--- airbean_react/src/views/Menu.js
+++ airbean_react/src/views/Menu.js
@@ -1,6 +1,17 @@
+import './Menu.css';
+
+import graphicsTop from '../assets/graphics/graphics-header.svg'
+import graphicsBotton from '../assets/graphics/graphics-footer.svg'
+
 function Menu () {
   return(
-    <h1>Menu page</h1>
+    <div className="menu-container">
+      <img src={ graphicsTop} />
+      <div className='menu'>
+        <h1 className='header'>Meny</h1>
+      </div>
+      <img src={ graphicsBotton } />
+    </div>
   )
 }
 
