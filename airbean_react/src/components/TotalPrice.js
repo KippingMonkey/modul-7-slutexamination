9c0e1a07b7e3919f50e972a950efa01bc0237c27
Jonas <jonas@kippingmonkey.se>
2022-03-21 00:19:33 +0100

    add functionality to totalprice

--- airbean_react/src/components/TotalPrice.js
+++ airbean_react/src/components/TotalPrice.js
@@ -1,6 +1,18 @@
 import './TotalPrice.css';
+import { useSelector } from 'react-redux';
 
-function TotalPrice (props){
+function TotalPrice (){
+  const cartItems = useSelector((state) => { return state.cartReducer.cartItems })
+  let totalPrice;
+  if (cartItems.length === 0 ) {
+      totalPrice = 0;
+  }
+  else{
+    const prices = cartItems.map(cartItem => cartItem.price);
+    console.log('prices', prices);
+    totalPrice = prices.reduce((a, b) => a + b);
+  }
+
   return(
     <div className="totalPrice-container">
       <div className='priceText-container'>
@@ -8,7 +20,7 @@ function TotalPrice (props){
         <p className="inclusive">inkl moms + drönarleverans</p>
       </div>
       <div className='price-container'>
-        <p className='price'>4345 kr</p>
+        <p className='price'>{ totalPrice } kr</p>
       </div>
     </div>
   )
